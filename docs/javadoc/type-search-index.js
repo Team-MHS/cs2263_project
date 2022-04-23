5bed23c7a67c8ff93caba1b19caa1fb0169606f7
Michael <esqumich@isu.edu>
2022-04-23 15:10:40 -0600

    committing javadoc feature

---
+++ docs/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"hw04","l":"App"},{"p":"hw04","l":"Corporation"},{"p":"hw04","l":"DequeImp"},{"p":"hw04","l":"Game"},{"p":"hw04","l":"Player"},{"p":"hw04","l":"Share"},{"p":"hw04","l":"Tile"},{"p":"hw04","l":"TileList"},{"p":"hw04","l":"UserInterface"}];updateSearchResults();
